754966a105ccaafdb982df8ef3b318c5513e4569
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-03-09 11:50:40 -0300

    ponteiro struct

--- struct.go
+++ struct.go
@@ -10,6 +10,10 @@ func main() {
 
 	var position1 = Position{0, 0}
 	var position2 = Position{3, 2}
-
 	fmt.Println(position1, position2)
+	
+	// ponteiro para struct
+	var k *Position = &position2
+	k.X = 33
+	fmt.Println(k.X)
 }
