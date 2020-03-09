f8af13c71a700725fe7db8cb27a97583a49c878c
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-03-09 10:24:35 -0300

    correção de nome da função

--- main.go
+++ main.go
@@ -4,22 +4,20 @@ import "fmt"
 
 func main() {
 	welcome()
-	
 	// declaration typed variable
 	var number1 float64 = 3.2
 	var number2 float64 = 2
 	// abbreviation of variable declaration
-	result := somaFloat(number1, number2)
+	result := sumFloat(number1, number2)
 	// print with some args
 	fmt.Println("Sum of two numbers: ",number1," + ",number2," = ", result)
-
 }
 
 func welcome() {
 	fmt.Println("Welcome to my first Code on Golang:")
 }
 
-func somaFloat(x float64, y float64) float64 {
+func sumFloat(x float64, y float64) float64 {
 	return x + y
 }
 
