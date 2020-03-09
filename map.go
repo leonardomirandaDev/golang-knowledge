0043a5179c7d1c862e8ef6c49537cfdfb3afa597
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-03-09 12:05:02 -0300

    map literal

---
+++ map.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notas map[string]int = make(map[string]int)
+
+	notas["nico"] = 10
+	notas["leo"] = 9
+
+	fmt.Println(notas)
+	
+	
+	// map literal ---------
+	
+	notas2 := map[string]int{
+		"nico": 7,
+		"leo": 6,
+	}
+
+
+	fmt.Println(notas2)
+}
